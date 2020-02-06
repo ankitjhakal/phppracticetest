80c093a669d077276cb30ee32adb1afae6026628
ankitjhakal <48992285+ankitjhakal@users.noreply.github.com>
2020-02-06 18:22:12 +0530

    Add files via upload

---
+++ rps.php
@@ -0,0 +1,11 @@
+<!-- user's choice in game -->
+<?php
+
+      echo '<form action="game.php" method="post" />
+            <input type="radio" name="User" value="Rock" title="Rock" />Rock <br /><br />
+            <input type="radio" name="User" value="Paper" title="Paper" />Paper <br /><br />
+            <input type="radio" name="User" value="Scissors" title="Scissors" />Scissors <br /><br />
+            <input type="submit" name="form_submit" value="submit"/>
+            </form> ';
+
+?>
